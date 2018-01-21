d6f24b986adb33e374f12bde24be6305c7c530b2
Marcus Blake <mab2401@columbia.edu>
2018-01-20 19:24:58 -0500

    fix 1

--- stuff.js
+++ stuff.js
@@ -1,8 +1,18 @@
+//* eslint-disable  func-names */
+/* eslint quote-props: ["error", "consistent"]*/
+/**
+ * This sample demonstrates a simple skill built with the Amazon Alexa Skills
+ * nodejs skill development kit.
+ * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
+ * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
+ * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
+ **/
+
 'use strict';
-const Alexa=require('alexa-sdk'); 
 
-const APP_ID=null; //Fill this in 
+const Alexa = require('alexa-sdk');
 
+var investmentRiskScore=0;
 const START_MESSAGE="Okay. I will give you some financial advice";
 
 const TIME_HORIZONQ1="When do you plan to begin withdrawing from your investment?";
@@ -11,7 +21,7 @@ const TIME_HORIZONQ2="Once you begin withdrawing funds from your investments whe
 
 const SCORE="Your current score is ";
 
-var investmentRiskScore=0;
+const APP_ID = "amzn1.ask.skill.75b9c99e-bdee-4bbf-8719-34b045597014";  // TODO replace with your app ID (OPTIONAL).
 
 //Declares the states
 const states={
@@ -19,19 +29,21 @@ const states={
 	ADVICE: '_ADVICEMODE'
 };
 
-const startHandlers=Alexa.CreateStateHander(states.START,
+
+const startHandlers=Alexa.CreateStateHandler(states.START, {
 
 	//This should prompt Alexa to start giving financial advice
-	"StartFinancialAdvice": function(){
+	'StartFinancialAdvice': function(){
 		this.response.speak(START_MESSAGE);
 		//Not quite sure why this, but it's here in case it's needed
 		this.emit(":responseReady"); 
-	}
+	    }
+    }
 );
 
-const adviceHandlers=Alexa.CreateStatHander(states.ADVICE,
+const adviceHandlers=Alexa.CreateStateHandler(states.ADVICE,{
 
-	"TimeHorizon": function(){
+	'TimeHorizon': function(){
 		this.response.speak(TIME_HORIZONQ1);
 		this.emit(":responseReady");
 
@@ -49,24 +61,23 @@ const adviceHandlers=Alexa.CreateStatHander(states.ADVICE,
 		}
 
 		this.response.speak(SCORE + investmentRiskScore);
-	};
+	}
 
+    }   
 );
 
 
-function addScore(var score){
+function addScore(score){
 	investmentRiskScore+=score;
 }
 
 
 
 
-exports.handler = (event, context) => {
-
-const alexa=Alexa.handler(event, context, callback);
-alexa.appId=APP_ID;
-alexa.registerHandlers(handlers, startHandlers, adviceHandlers);
-alexa.execute();
-	
-
-}
+exports.handler = function (event, context) {
+    const alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    
+    alexa.registerHandlers(startHandlers, adviceHandlers);
+    alexa.execute();
+};
